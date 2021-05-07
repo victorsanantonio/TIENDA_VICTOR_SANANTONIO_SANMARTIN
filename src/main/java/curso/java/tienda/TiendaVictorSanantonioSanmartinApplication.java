1d47542e8c485757730204b439ba8fb799b5352a
victorsanantonio <victorsanantonio@hotmail.com>
2021-05-07 13:18:44 +0200

    Commit inicial

---
+++ src/main/java/curso/java/tienda/TiendaVictorSanantonioSanmartinApplication.java
@@ -0,0 +1,13 @@
+package curso.java.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaVictorSanantonioSanmartinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaVictorSanantonioSanmartinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/curso/java/tienda/TiendaVictorSanantonioSanmartinApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.java.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVictorSanantonioSanmartinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
